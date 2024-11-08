8b21f22ec0d9b73a1093287824a45744003d4b61
JulyLun <julylun.cat@gmail.com>
2024-11-08 22:22:23 +0700

    add basic folder; Add siderbar.html

--- public/tailwind.config.js
+++ public/tailwind.config.js
@@ -1,9 +1,31 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content:["./**/*.{html,js}"],
   theme: {
-    extend: {},
+    colors: {
+      transparent: 'transparent',
+      'background': '#170F23',
+      'side_bar_background':'#231B2E'
+    },
+    extend: {
+      textStrokeWidth: {
+        '1':'1px',
+        '2':'2px',
+      }
+    },
   },
-  plugins: [],
+  plugins: [
+     function ({ addUtilities }) {
+      addUtilities({
+        '.text-stroke': {
+          '-webkit-text-stroke': '1px white',
+        },
+        '.text-stroke-2': {
+          '-webkit-text-stroke': '2px white',
+        },
+      })
+    }, 
+
+  ],
 }
 
